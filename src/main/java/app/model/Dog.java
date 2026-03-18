1f49300122f55dd2d3374bb285a89413d9e859d5
Vladyslava <vladislavak33@gmail.com>
2026-03-19 01:53:24 +0900

    completed project2.1

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component ("dog")
+public class Dog extends Animal {
+    @Override
+    public String toString() {return "Im a Dog";}
+}
